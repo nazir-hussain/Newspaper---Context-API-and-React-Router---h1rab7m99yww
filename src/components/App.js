737cf9bd0039b66bc0867fa7b25e4bf3df381dbe
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 15:24:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,46 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useEffect, useState } from "react";
+import "../styles/App.css";
+
+
+const weatherComponent = () => {
+  const [lat, setLat] = useState([]);
+  const [lng, setLng] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        setLat(pos.coords.latitude);
+        setLng(pos.coords.longitude);
+      });
+
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=f1a6a9f4b1cca85753aa1d87e71cba3a`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, lng]);
+
+  return (
+    <>
+      Weather and News
+      {typeof data.main != "undefined" ? <Weather data={data} /> : <div></div>}
+    </>
+  );
+};
 
 const App = () => {
   return (
-    <></>
+    <><h1>
+      React Project
+    </h1>
+    <weatherComponent />
+    </>
   )
 }
 
